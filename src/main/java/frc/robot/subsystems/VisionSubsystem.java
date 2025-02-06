7c320d1b130b8fe6852311f2338fad16bbdd55f2
Doug Wegscheid <dwegscheid@sbcglobal.net>
2025-02-06 14:19:17 -0500

    Only read pose data from limelight when it's updated.

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -1,38 +1,100 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.usfirst.frc3620.NTPublisher;
 import org.usfirst.frc3620.NTStructs;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.networktables.DoubleArrayEntry;
+import edu.wpi.first.networktables.NetworkTableEvent;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.PoseEstimate;
 
 public class VisionSubsystem extends SubsystemBase {
-  String llNameFront = "limelight-front";
-  String llNameBack = "limelight-back";
+  NetworkTableInstance inst = NetworkTableInstance.getDefault();
 
-  String bluePoseTopicName = "peBlue";
+  public enum Camera {
+    FRONT("limelight-front"), BACK("limelight-back");
 
-  /** Creates a new VisionSubsystem. */
+    public final String limelightName;
+
+    Camera(String limelightName) {
+      this.limelightName = limelightName;
+    }
+  }
+
+  public class CameraData {
+    // gets set whenever we have new data
+    AtomicBoolean haveNewMetatag1Pose = new AtomicBoolean(false);
+
+    Pose2d metatag1Pose;
+    double metatag1Timestamp;
+    int metatag1TagCount;
+
+    CameraData(Camera c) {
+      /*
+       * see
+       * https://docs.wpilib.org/en/stable/docs/software/networktables/listening-for-change.html#using-networktableinstance-to-listen-for-changes
+       * 
+       * we set the haveNew* atomic(s) when the corresponding network entry is updated,
+       * and in periodic(), only
+       * go through all the work of reading it if we got a new one.
+       */
+      DoubleArrayEntry entry = LimelightHelpers.getLimelightDoubleArrayEntry(c.limelightName, "botpose_wpiblue");
+      inst.addListener(
+          entry,
+          EnumSet.of(NetworkTableEvent.Kind.kValueAll),
+          event -> haveNewMetatag1Pose.set(true));
+    }
+
+    public Pose2d getMetatag1Pose() {
+      return metatag1Pose;
+    }
+
+    public double getMetatag1Timestamp() {
+      return metatag1Timestamp;
+    }
+
+    public int getMetatag1TagCount() {
+      return metatag1TagCount;
+    }
+  }
+
+  Map<Camera, CameraData> cameraData = new TreeMap<>();
+
   public VisionSubsystem() {
+    cameraData.put(Camera.FRONT, new CameraData(Camera.FRONT));
+    cameraData.put(Camera.BACK, new CameraData(Camera.BACK));
   }
 
   @Override
   public void periodic() {
-    updateFromCamera(llNameFront);
-    updateFromCamera(llNameBack);
+    for (var entry : cameraData.entrySet()) {
+      var cameraData = entry.getValue();
+
+      if (cameraData.haveNewMetatag1Pose.getAndSet(false)) {
+        var limelightName = entry.getKey().limelightName;
+        PoseEstimate m = LimelightHelpers.getBotPoseEstimate_wpiBlue(limelightName);
+
+        if (m != null) {
+          cameraData.metatag1Pose = m.pose;
+          cameraData.metatag1Timestamp = m.timestampSeconds;
+          cameraData.metatag1TagCount = m.tagCount;
+
+          var prefix = "SmartDashboard/frc3620/vision/" + limelightName + "/";
+          NTPublisher.putNumber(prefix + "targetCount", m.tagCount);
+          NTStructs.publish(prefix + "poseEstimate", m.pose);
+        }
+      }
+    }
   }
 
-  void updateFromCamera(String ll) {
-    String prefix = "SmartDashboard/frc3620/vision/" + ll + "/";
-    LimelightHelpers.PoseEstimate m = LimelightHelpers.getBotPoseEstimate_wpiBlue(ll);
-    if (m != null) {
-      NTPublisher.putNumber(prefix + "targetCount", m.tagCount);
-      NTStructs.publish(prefix + bluePoseTopicName, m.pose);
-    }
+  public CameraData getCameraData(Camera camera) {
+    return cameraData.get(camera);
   }
 
 }
