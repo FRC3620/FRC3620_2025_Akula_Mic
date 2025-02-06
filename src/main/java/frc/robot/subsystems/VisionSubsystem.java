28ff498d6ce0d0259fb71d8cf6e41320a800d839
Doug Wegscheid <dwegscheid@sbcglobal.net>
2025-02-06 12:51:44 -0500

    Break out code that puts structs into Network Tables.

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -4,13 +4,9 @@
 
 package frc.robot.subsystems;
 
-import java.util.*;
+import org.usfirst.frc3620.NTPublisher;
+import org.usfirst.frc3620.NTStructs;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.StructPublisher;
-import edu.wpi.first.util.struct.Struct;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.LimelightHelpers;
 
@@ -19,14 +15,9 @@ public class VisionSubsystem extends SubsystemBase {
   String llNameBack = "limelight-back";
 
   String bluePoseTopicName = "peBlue";
-  String redPoseTopicName = "peRed";
-
-  Map<String, StructPublisher<? extends Object>> publishers = new TreeMap<>();
 
   /** Creates a new VisionSubsystem. */
   public VisionSubsystem() {
-    makePublishers(llNameFront);
-    makePublishers(llNameBack);
   }
 
   @Override
@@ -36,42 +27,12 @@ public class VisionSubsystem extends SubsystemBase {
   }
 
   void updateFromCamera(String ll) {
-    String prefix = "frc3620/vision/" + ll + "/";
+    String prefix = "SmartDashboard/frc3620/vision/" + ll + "/";
     LimelightHelpers.PoseEstimate m = LimelightHelpers.getBotPoseEstimate_wpiBlue(ll);
-    SmartDashboard.putNumber(prefix + "targetCount", m.tagCount);
-    publish(ll, bluePoseTopicName, m.pose);
-
-    m = LimelightHelpers.getBotPoseEstimate_wpiRed(ll);
-    SmartDashboard.putNumber(prefix + "targetCount", m.tagCount);
-    publish(ll, redPoseTopicName, m.pose);
-
-  }
-
-  void makePublishers(String ll) {
-    makePublisher(ll, bluePoseTopicName, Pose2d.struct);
-    makePublisher(ll, redPoseTopicName, Pose2d.struct);
-  }
-
-  void makePublisher(String ll, String topicName, Struct<?> s) {
-    String fullTopicName = fullTopicName(ll, topicName);
-    StructPublisher<? extends Object> publisher = NetworkTableInstance.getDefault().getStructTopic(fullTopicName, s).publish();
-    publishers.put(fullTopicName, publisher);
-  }
-
-  @SuppressWarnings({ "rawtypes", "unchecked" })
-  void publish(String ll, String topicName, Object o) {
-    String fullTopicName = fullTopicName(ll, topicName);
-    StructPublisher publisher = publishers.get(fullTopicName);
-    if (publisher != null) {
-      publisher.set(o);
+    if (m != null) {
+      NTPublisher.putNumber(prefix + "targetCount", m.tagCount);
+      NTStructs.publish(prefix + bluePoseTopicName, m.pose);
     }
   }
 
-  String fullTopicName(String ll, String topicName) {
-    StringBuilder sb = new StringBuilder("SmartDashboard/frc3620/vision/");
-    sb.append(ll);
-    sb.append("/");
-    sb.append(topicName);
-    return sb.toString();
-  }
 }
