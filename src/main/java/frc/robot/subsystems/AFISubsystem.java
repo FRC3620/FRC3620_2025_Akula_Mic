6e3eb0472fc79fdcef6b2fffa273b4136e7aec27
Albert <12158@stjoebears.com>
2025-02-11 20:00:20 -0500

    We are able to get a reading. The issue is the absolute encoder takes a long time to validate. while unvalidated, it gives us a weird number.

--- src/main/java/frc/robot/subsystems/AFISubsystem.java
+++ src/main/java/frc/robot/subsystems/AFISubsystem.java
@@ -7,7 +7,9 @@ package frc.robot.subsystems;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Rotations;
 
+import org.tinylog.TaggedLogger;
 import org.usfirst.frc3620.CANDeviceType;
+import org.usfirst.frc3620.logger.LoggingMaster;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
@@ -18,93 +20,128 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
 
 public class AFISubsystem extends SubsystemBase {
   /** Creates a new AFISubsystem. */
-    TalonFXConfiguration pivotConfig = new TalonFXConfiguration();
-    public TalonFX pivot;
-    public DutyCycleEncoder frontEncoder;
-    public DutyCycleEncoder rearEncoder;
+  TaggedLogger logger = LoggingMaster.getLogger(getClass());
+  TalonFXConfiguration pivotConfig = new TalonFXConfiguration();
+  public TalonFX pivot;
+  public DutyCycleEncoder frontEncoder;
+  public DutyCycleEncoder rearEncoder;
 
-    final PositionVoltage pivotRequest = new PositionVoltage(0).withSlot(0);
-    final DutyCycleOut rollerRequest = new DutyCycleOut(0);
+  enum WhichEncoderToUse {
+    FRONT, REAR
+  }
 
+  WhichEncoderToUse whichEncoderToUse = WhichEncoderToUse.REAR;
 
-    //SparkMaxConfig rollerConfig = new SparkMaxConfig();
-    public TalonFX roller;
+  boolean relativeEncoderSet = false;
 
-    final int AFIPIVOTMOTORID = 14;
-    final int AFIROLLERMOTORID = 15;
+  Timer relativeEncoderTimer = new Timer();
 
-    // this is the ratio of motor rotations to intake arm rotations
-    final static double MOTOR_TO_INTAKE_RATIO = 5 * 5 * (3.0 / 2.0);
-    
-  
+  final PositionVoltage pivotRequest = new PositionVoltage(0).withSlot(0);
+  final DutyCycleOut rollerRequest = new DutyCycleOut(0);
+
+  // SparkMaxConfig rollerConfig = new SparkMaxConfig();
+  public TalonFX roller;
+
+  final int AFIPIVOTMOTORID = 14;
+  final int AFIROLLERMOTORID = 15;
+
+  // this is the ratio of motor rotations to intake arm rotations
+  final static double MOTOR_TO_INTAKE_RATIO = 5 * 5 * (3.0 / 2.0);
 
   public AFISubsystem() {
-    //constructor
+    // constructor
+    this.frontEncoder = new DutyCycleEncoder(5); // Down is .292
+    this.rearEncoder = new DutyCycleEncoder(6);
+    relativeEncoderTimer.reset();
+    relativeEncoderTimer.start();
 
-  
-    //Pivot
-     if (RobotContainer.canDeviceFinder.isDevicePresent(CANDeviceType.TALON_PHOENIX6, AFIPIVOTMOTORID, "AFIPivot")
-                || RobotContainer.shouldMakeAllCANDevices()) {
-            this.frontEncoder = new DutyCycleEncoder(5); //Down is .292
-            this.rearEncoder = new DutyCycleEncoder(6);
-            this.pivot = new TalonFX(AFIPIVOTMOTORID);
-            // this.shoulderEncoder = new CANcoder(10);
-            Slot0Configs slot0Configs = new Slot0Configs();
-            slot0Configs.kG = 0; // Gravity FeedForward
-            slot0Configs.kS = 0; // Friction FeedForward
-            slot0Configs.kP = 1; // an error of 1 rotation results in x Volt output
-            slot0Configs.kI = 0;
-            slot0Configs.kD = 0;
+    // Pivot
+    if (RobotContainer.canDeviceFinder.isDevicePresent(CANDeviceType.TALON_PHOENIX6, AFIPIVOTMOTORID, "AFIPivot")
+        || RobotContainer.shouldMakeAllCANDevices()) {
+      this.pivot = new TalonFX(AFIPIVOTMOTORID);
+      // this.shoulderEncoder = new CANcoder(10);
+      Slot0Configs slot0Configs = new Slot0Configs();
+      slot0Configs.kG = 0; // Gravity FeedForward
+      slot0Configs.kS = 0; // Friction FeedForward
+      slot0Configs.kP = 1; // an error of 1 rotation results in x Volt output
+      slot0Configs.kI = 0;
+      slot0Configs.kD = 0;
 
-            pivot.getConfigurator().apply(slot0Configs);
+      pivot.getConfigurator().apply(slot0Configs);
 
-            pivot.setPosition(AbsoluteIntakeAngle().times(MOTOR_TO_INTAKE_RATIO));
-          } // Applies the Config to the shoulder motor
+    } // Applies the Config to the shoulder motor
 
-      //Roller
-      if (RobotContainer.canDeviceFinder.isDevicePresent(CANDeviceType.TALON_PHOENIX6, AFIROLLERMOTORID, "AFIRoller")
-                || RobotContainer.shouldMakeAllCANDevices()) {
-            this.roller = new TalonFX(AFIROLLERMOTORID);
-          } 
-          
+    // Roller
+    if (RobotContainer.canDeviceFinder.isDevicePresent(CANDeviceType.TALON_PHOENIX6, AFIROLLERMOTORID, "AFIRoller")
+        || RobotContainer.shouldMakeAllCANDevices()) {
+      this.roller = new TalonFX(AFIROLLERMOTORID);
+    }
+
   }
 
   @Override
   public void periodic() {
+    if (!relativeEncoderSet) {
+      if (relativeEncoderTimer.hasElapsed(5)) {
+        if (isEncoderConnected()) {
+          logger.info("AFI absolute angle is {}", getAbsoluteIntakeAngle().in(Degrees));
+          logger.info("AFI relative angle is before {}", pivot.getPosition().getValue().in(Degrees));
+          logger.info("setting the posistion {}", getAbsoluteIntakeAngle().times(MOTOR_TO_INTAKE_RATIO).in(Degrees));
+          pivot.setPosition(getAbsoluteIntakeAngle().times(MOTOR_TO_INTAKE_RATIO));
+          logger.info("AFI relative angle is after {}", pivot.getPosition().getValue().in(Degrees));
+
+          relativeEncoderSet = true;
+        }
+      }
+    }
+    // logger.info("AFI absolute angle is {}",
+    // getAbsoluteIntakeAngle().in(Degrees));
+
     // This method will be called once per scheduler run
-     if (pivot != null) {
-            SmartDashboard.putNumber("frc3620/AFI/PivotActualPosition", AbsoluteIntakeAngle().in(Degrees));
-            SmartDashboard.putNumber("frc3620/AFI/PivotRelativePosition", pivot.getPosition().getValue().div(MOTOR_TO_INTAKE_RATIO).in(Degrees));
-        }
+    if (pivot != null) {
+      SmartDashboard.putNumber("frc3620/AFI/PivotActualPosition", getAbsoluteIntakeAngle().in(Degrees));
+      SmartDashboard.putNumber("frc3620/AFI/PivotRelativePosition",
+          pivot.getPosition().getValue().div(MOTOR_TO_INTAKE_RATIO).in(Degrees));
+    }
+  }
+
+  public void setPivotPosition(Angle position) {
+    // set the shoulder to the desired position Cat
+    SmartDashboard.putNumber("frc3620/AFI/PivotRequestedPosition", position.in(Degrees));
+
+    if (pivot != null) {
+      pivot.setControl(pivotRequest.withPosition(position.times(MOTOR_TO_INTAKE_RATIO)));
+    }
+  }
+
+  boolean isEncoderConnected() {
+    if (whichEncoderToUse == WhichEncoderToUse.FRONT) {
+      return frontEncoder.isConnected();
+    } else {
+      return rearEncoder.isConnected();
+    }
+  }
+
+  Angle rearEncoderOffset = Degrees.of(200);
+  Angle frontEncoderOffset = Degrees.of(0);
+
+  public Angle getAbsoluteIntakeAngle() {
+    if (whichEncoderToUse == WhichEncoderToUse.FRONT) {
+      return Rotations.of(frontEncoder.get()).minus(frontEncoderOffset);
+    } else {
+      return Rotations.of(rearEncoder.get()).minus(rearEncoderOffset);
     }
 
-    public void setPivotPosition(Angle position) {
-        // set the shoulder to the desired position Cat
-        SmartDashboard.putNumber("frc3620/AFI/PivotRequestedPosition", position.in(Degrees));
+  }
 
-        if (pivot != null) {
-            pivot.setControl(pivotRequest.withPosition(position.times(MOTOR_TO_INTAKE_RATIO)));
-        }
-    }
-
-    double frontEncoderOffset = 0.5575;
-    public Angle AbsoluteIntakeAngle() {
-      Angle rv = Rotations.of(rearEncoder.get() - frontEncoderOffset);
-      return rv;
-
-    }
-
-    public Angle absoluteIntakeAngle() {
-      return AbsoluteIntakeAngle();
-    }
-
-  public void setAFIRollerSpeed(double speed){
+  public void setAFIRollerSpeed(double speed) {
     roller.setControl(rollerRequest.withOutput(speed));
   }
 }
