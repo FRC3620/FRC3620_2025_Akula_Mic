5d344ce697d6815c064a0f8a43c6c2b202b17fa8
Logan Webb <loganjwebb13@gmail.com>
2025-02-14 23:06:08 -0500

    Swaped Raycast for  Winding Number Algorithm

--- src/main/java/frc/robot/ShapeBoundary.java
+++ src/main/java/frc/robot/ShapeBoundary.java
@@ -1,58 +1,42 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import java.util.ArrayList;
 import java.util.List;
-import edu.wpi.first.math.geometry.Translation2d;
 
-/** Add your docs here. */
 public class ShapeBoundary {
-
-        //https://chat.deepseek.com/a/chat/s/b83653a5-875b-4310-a834-81804759d1cb
-
     private List<Translation2d> vertices;
 
     public ShapeBoundary() {
-        vertices = new ArrayList<>();
-        // Define the vertices of the rectangle
-        vertices.add(new Translation2d(0, 0));       // Bottom-left corner
-        vertices.add(new Translation2d(0, 5));       // Top-left corner
-        vertices.add(new Translation2d(10, 5));       // Top-right corner
-        vertices.add(new Translation2d(10, 0));      // Bottom-right corner
+        this.vertices = vertices;
     }
 
-    
     public List<Translation2d> getVertices() {
         return vertices;
     }
 
-    public boolean isPointInside(Translation2d point, List<Translation2d> vertices) {
-
+    public boolean isPointInside(Translation2d point,List<Translation2d> vertices) {
+        int windingNumber = 0;
         int numVertices = vertices.size();
-        boolean inside = false;
-
-        // Loop through each edge of the polygon
-        for (int i = 0, j = numVertices - 1; i < numVertices; j = i++) {
-            Translation2d vertex1 = vertices.get(i); // Current vertex
-            Translation2d vertex2 = vertices.get(j); // Previous vertex
-
-            // Check if the point is within the y-range of the edge
-            if (((vertex1.getY() > point.getY()) != (vertex2.getY() > point.getY())) &&
-                // Check if the point is to the left of the edge's intersection with the ray
-                (point.getX() < (vertex2.getX() - vertex1.getX()) * (point.getY() - vertex1.getY()) / (vertex2.getY() - vertex1.getY()) + vertex1.getX())) {
-                inside = true; 
+        
+        for (int i = 0; i < numVertices; i++) {
+            Translation2d v1 = vertices.get(i);
+            Translation2d v2 = vertices.get((i + 1) % numVertices);
+            
+            if (v1.getY() <= point.getY()) {
+                if (v2.getY() > point.getY() && isLeft(v1, v2, point) > 0) {
+                    windingNumber++;
+                }
+            } else {
+                if (v2.getY() <= point.getY() && isLeft(v1, v2, point) < 0) {
+                    windingNumber--;
+                }
             }
         }
-            SmartDashboard.putBoolean("Inside Shape", inside);
-    return inside;
-}
-}
+        
+        return windingNumber != 0; // Nonzero means inside
+    }
 
-
+    private double isLeft(Translation2d a, Translation2d b, Translation2d p) {
+        return (b.getX() - a.getX()) * (p.getY() - a.getY()) - (b.getY() - a.getY()) * (p.getX() - a.getX());
+    }
+}
---
+++ src/test/java/ShapeTestOne.java
@@ -0,0 +1,29 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.ShapeBoundary;
+
+public class ShapeTestOne {
+    List<Translation2d> vertices = new ArrayList<>();
+    ShapeBoundary shape = new ShapeBoundary();
+
+    @Test
+    public void triangleTest() {
+        List<Translation2d> triangle = new ArrayList<>();
+        triangle.add(new Translation2d(4.5, 2.5));
+        triangle.add(new Translation2d(7.4, 8.4));
+        triangle.add(new Translation2d(1.8, 8.2));
+
+        Translation2d point = new Translation2d(3.2, 2.7);
+
+        boolean inside = shape.isPointInside(point, triangle);
+        System.out.println ("triangle = " + triangle + ", point = " + point + ", inside = " + inside);
+        Assertions.assertFalse(inside);
+    }
+}
