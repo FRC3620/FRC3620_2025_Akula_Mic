248dead2df4eebb1f548be543eb97aa1ad52f571
AJ <10586@stjoebears.com>
2025-02-16 15:30:46 -0500

    Need to keep hitting the motor to keep it running!

--- src/main/java/frc/robot/commands/esefcommands/RunEndEffectorUntilHasCoral.java
+++ src/main/java/frc/robot/commands/esefcommands/RunEndEffectorUntilHasCoral.java
@@ -29,6 +29,7 @@ public class RunEndEffectorUntilHasCoral extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    esefSubsystem.setEndEffSpeed(speed);
   }
 
   // Called once the command ends or is interrupted.
