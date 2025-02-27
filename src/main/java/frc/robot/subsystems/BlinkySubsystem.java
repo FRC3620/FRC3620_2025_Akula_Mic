63886463b5e2a1aa9c944b897cae20a922cc6b65
Doug Wegscheid <dwegscheid@sbcglobal.net>
2025-02-26 23:08:20 -0500

    Blinky got easier in 2025.

--- src/main/java/frc/robot/subsystems/BlinkySubsystem.java
+++ src/main/java/frc/robot/subsystems/BlinkySubsystem.java
@@ -1,166 +1,74 @@
 package frc.robot.subsystems;
 
-import java.util.ArrayList;
-import java.util.List;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Value;
+
 import org.tinylog.TaggedLogger;
 import org.usfirst.frc3620.logger.LoggingMaster;
-import org.usfirst.frc3620.BlinkinColor;
 
+import edu.wpi.first.units.measure.Dimensionless;
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.AddressableLEDBufferView;
+import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.blinky.Pattern;
 
 /**/
 public class BlinkySubsystem extends SubsystemBase {
   TaggedLogger logger = LoggingMaster.getLogger(getClass());
 
-  AddressableLED leds;
-  AddressableLEDBuffer lBuffer;
-  Timer timer;
-  List<LightSegment> lightSegments = new ArrayList<>();
+  static final Dimensionless BRIGHTNESS = Value.of(0.1);
 
-  Spark spark, s2;
+  AddressableLED addressableLED;
+  AddressableLEDBuffer addressableLEDBuffer;
 
-  BlinkinColor color = null;
+  AddressableLEDBufferView left, right, top;
+  LEDPattern leftPattern, rightPattern, topPattern;
 
-  boolean runningIntakeAtSource = false;
-  boolean readyToShoot = false;
+  private final LEDPattern m_rainbow = LEDPattern.rainbow(255, 32);
+  // Our LED strip has a density of 120 LEDs per meter
+  private static final Distance kLedSpacing = Meters.of(1 / 120.0);
 
-  /** Creates a new BlinkySubsystem. */
+  // Create a new pattern that scrolls the rainbow pattern across the LED strip,
+  // moving at a speed
+  // of 1 meter per second.
+  private final LEDPattern PATTERN_SCROLLING_RAINBOW = m_rainbow
+      .scrollAtAbsoluteSpeed(MetersPerSecond.of(.01), kLedSpacing).atBrightness(BRIGHTNESS);
+
+  private final LEDPattern PATTERN_RED = LEDPattern.solid(Color.kRed).atBrightness(BRIGHTNESS);
+  private final LEDPattern PATTERN_BLUE = LEDPattern.solid(Color.kBlue).atBrightness(BRIGHTNESS);
+
+  private final LEDPattern PATTERN_POLICE = PATTERN_BLUE.blink(Seconds.of(1)).overlayOn(PATTERN_RED);
+
   public BlinkySubsystem() {
-    spark = new Spark(1);
-    s2 = new Spark(2);
-    spark.addFollower(s2);
+    addressableLED = new AddressableLED(0);
+    addressableLEDBuffer = new AddressableLEDBuffer(6);
 
-    leds = new AddressableLED(0);
-    lBuffer = new AddressableLEDBuffer(20);
-    timer = new Timer();
-    leds.setLength(lBuffer.getLength());
-    leds.start();
+    addressableLED.setLength(addressableLEDBuffer.getLength());
+    addressableLED.setData(addressableLEDBuffer);
+    addressableLED.start();
+
+    left = addressableLEDBuffer.createView(0, 1);
+    right = addressableLEDBuffer.createView(2, 3);
+    top = addressableLEDBuffer.createView(4, 5);
+
+    leftPattern = PATTERN_SCROLLING_RAINBOW;
+    rightPattern = PATTERN_POLICE;
+    topPattern = PATTERN_RED;
   }
 
   @Override
   public void periodic() {
-    boolean changed = false;
-    for (var lightSegment : lightSegments) {
-      if (lightSegment.runPattern()) {
-        changed = true;
-      }
-    }
-    if (changed) {
-      leds.setData(lBuffer);
-    }
+    leftPattern.applyTo(left);
+    rightPattern.applyTo(right);
+    topPattern.applyTo(top);
 
-    blinkinPeriodic();
+    // Set the LEDs
+    addressableLED.setData(addressableLEDBuffer);
   }
 
-  void blinkinPeriodic() {
-    if (getCurrentCommand() == null) {
-      BlinkinColor c = BlinkinColor.BLUE;
-    
-      setBlinkin(c);
-    } else {
-      if (color == null) {
-        setBlinkin(null);
-      } else {
-        setBlinkin(color);
-      }
-    }
-  }
-
-  BlinkinColor currentBlinkinColor = null;
-  void setBlinkin(BlinkinColor c) {
-    if (currentBlinkinColor != c) {
-      logger.info ("Changing Blinkin to {}", c);
-      if (c == null) {
-        spark.disable();
-      } else {
-        spark.set(c.getPower());
-      }
-      currentBlinkinColor = c;
-    }
-  }
-
-  public LightSegment getLightSegment(int first, int last) {
-    LightSegment rv = new LightSegment(first, last);
-    lightSegments.add(rv);
-    return rv;
-  }
-
-  public void setRunningIntakeAtSource(boolean b) {
-    runningIntakeAtSource = b;
-  }
-
-  public void setReadyToShoot(boolean b) {
-    readyToShoot = b;
-  }
-
-  public void setColor(BlinkinColor color) {
-    this.color = color;
-  }
-
-  public class LightSegment extends SubsystemBase {
-    int segment_first;
-    int segment_length;
-
-    Pattern currentPattern;
-
-    boolean patternChanged = false;
-
-    public LightSegment(int first, int last) {
-      this.segment_first = first;
-      this.segment_length = (last - first) + 1;
-      setName(getName() + "[" + first + "-" + last + "]");
-    }
-
-    /**
-     * Define what pattern the light segment should display.
-     * 
-     * @param pattern
-     */
-    public void setPattern(Pattern pattern) {
-      if (pattern == currentPattern)
-        return;
-      // logger.info ("{} pattern set to {}", getName(), pattern.toString());
-      patternChanged = true;
-      if (currentPattern != null) {
-        currentPattern.done(this);
-      }
-      currentPattern = pattern;
-      currentPattern.start(this);
-    }
-
-    boolean runPattern() {
-      boolean rv = patternChanged;
-      patternChanged = false;
-      if (currentPattern != null) {
-        if (currentPattern.periodic(this)) {
-          rv = true;
-        }
-      }
-      return rv;
-    }
-
-    public void updateLEDs(Color color) {
-      updateLEDs(0, segment_length, color);
-    }
-
-    public void updateLEDs(int first, Color color) {
-      updateLEDs(first, first, color);
-    }
-
-    public void updateLEDs(int first, int last, Color color) {
-      for (int i = first; i < last; i++) {
-        lBuffer.setLED(i + segment_first, color);
-      }
-    }
-
-    public int getLength() {
-      return segment_length;
-    }
-  }
 }
